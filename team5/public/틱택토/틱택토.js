4b8fb9955137ead12589bb50fa9a2f9da6ec8236
504 <504@DESKTOP-G2B2I5O>
2023-06-27 16:42:00 +0900

    입력0627커밋차정훈

--- team5/public/틱택토/틱택토.js
+++ team5/public/틱택토/틱택토.js
@@ -1,3 +1,28 @@
-/**
- * 
- */
+//---------------------------------입력-------------------------------------------------
+
+//배열선언
+let board1 = []
+
+//처음 9개 버튼설정
+for(let i=1; i<=9; i++){
+	document.querySelector('#inputBox').innerHTML +=
+	`<button class = "input" onclick="inputNumber(${i})"></button>`
+	if(i%3==0){
+		document.querySelector('#inputBox').innerHTML += '<br/>'
+	}
+}
+
+//클릭시 클릭값 등록
+function inputNumber(num){
+	board1.indexOf(num) != -1 ? alert('중복입니다.'):board1.push(num)
+	
+	console.log(board1)
+	
+	if(board1.length==6){
+		board1.splice(0)
+		alert('초기화합니다.')
+	}
+}
+
+
+ 
