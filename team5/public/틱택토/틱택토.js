b73f62662ced46e2dfc9fff99445c9f5e00215c7
504 <504@DESKTOP-E6CD24T>
2023-06-27 16:39:45 +0900

    06_27 황태웅

--- team5/public/틱택토/틱택토.js
+++ team5/public/틱택토/틱택토.js
@@ -1,3 +1,37 @@
 /**
  * 
  */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// 초기화 함수
+function resetGame() {
+  board = ["", "", "", "", "", "", "", "", ""];
+  currentPlayer = "o";
+  clearBoard();
+}
+
+// 보드 초기화 함수
+function clearBoard() {
+  let buttons = document.getElementsByClassName(""); //  출력함수에서 button.classList.add("");기능에 해당하는 ""에 들어가는 값 
+  Array.from(buttons).forEach(button => {
+    button.textContent = "";
+  });
+}
+
+// 초기화 및 게임 시작
+resetGame();
+createButtons();
