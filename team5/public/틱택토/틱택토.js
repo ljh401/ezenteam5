b40e3785456938c85b2957b2d4cf627a4ee3bd09
504 <504@DESKTOP-3IJG99V>
2023-06-27 16:50:32 +0900

    06/27 16:50 정용상 업로드

--- team5/public/틱택토/틱택토.js
+++ team5/public/틱택토/틱택토.js
@@ -1,3 +1,20 @@
-/**
- * 
- */
+let board1 = [ `` , `` , ``,
+          	  `` , `` , ``,
+           	  `` , `` , ``]
+
+ttButton()
+
+function ttButton(){
+	console.log('함수 시작')
+	let ttTable = document.querySelector('.ttTable')
+	let ttHTML = ``;
+	for(let i=1; i<=board1.length; i++){
+		ttHTML += `<td><button 
+		style="border:none; background-color: #ffe9d0;"
+		type="button">${i-1}</button></td>`
+				
+		if(i%3==0){ ttHTML += `</tr>`}
+		
+	}
+	ttTable.innerHTML = ttHTML
+}
